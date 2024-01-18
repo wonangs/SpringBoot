af925beff522d8a6f1e8b7e4137e5dd5f25c66f4
wonangs <kyh1281@kangwon.ac.kr>
2024-01-18 17:55:41 +0900

    240118 1755

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Slf4j
@@ -75,9 +76,24 @@ public class MemberController {
         return "redirect:/members/" + memberEntity.getId();
     }
 
+    @PostMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다.");
+
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+        return "/redirect:/members";
+    }
+
 
     @PostMapping("/join")
     public String join(MemberForm memberForm) {
+
         log.info(memberForm.toString());
 
         Member member = memberForm.toEntity();
